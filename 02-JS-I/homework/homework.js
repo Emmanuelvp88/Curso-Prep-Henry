aa6c3059f02c207fcf20fdda92482b3d3e9a8842
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-04-10 21:55:24 -0600

    Termine el homework 02 JS

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -214,9 +214,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  euro = 1.20;
-  let cambio = euro * 5;
-  return cambio;
+ return euro * 1.20;
 }
 
 
@@ -226,7 +224,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u" ){
+    return "Es vocal"
+  }else if(letra.equals > 2){
+    return "Dato incorrecto"
+  }else{
+    return "Dato incorrecto"
+  }
 }
 
 
